81f3583f989beca25637e0810d7a6585b237fc12
yurchellow <onkelyura@gmail.com>
2020-02-28 23:16:42 +0300

    Max

---
+++ chapter_001/src/test/java/condition/TriangleTest.java
@@ -0,0 +1,21 @@
+package condition;
+
+import org.junit.Test;
+import ru.job4j.condition.Triangle;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist() {
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenExist1() {
+        boolean result = Triangle.exist(1.0, 2.0, 3.0);
+        assertThat(result, is(true));
+    }
+}
