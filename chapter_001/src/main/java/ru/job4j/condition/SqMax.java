69feec50fd3d888d9346bd85c6fb90346a3946a4
yurchellow <onkelyura@gmail.com>
2020-03-02 22:49:35 +0300

    loop

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -2,19 +2,21 @@ package ru.job4j.condition;
 
 public class SqMax {
     public static int max(int first, int second, int third, int forth) {
-        int result = third;
+        int result = forth;
         if (first > second) {
             if (first > third) {
                 if (first > forth) {
-                    result = third;
+                    result = first;
                 }
-            }
+            }else if (forth > first) {
+                        result = forth;
+                    }
         } else if (second > third) {
             if (second > forth) {
-                result = first;
+                result = second;
             }
         } else if (third > forth) {
-            result = second;
+            result = third;
         }
         return result;
     }
--- chapter_001/src/test/java/condition/SqMaxTest.java
+++ chapter_001/src/test/java/condition/SqMaxTest.java
@@ -12,4 +12,19 @@ public class SqMaxTest {
         int result = SqMax.max(6, 4, 7, 5);
         assertThat(result, is(7));
     }
+    @Test
+    public void maxt2() {
+        int result = SqMax.max(7, 6, 4, 5);
+        assertThat(result, is(7));
+    }
+    @Test
+    public void maxt3() {
+        int result = SqMax.max(5, 7, 4, 6);
+        assertThat(result, is(7));
+    }
+    @Test
+    public void maxt4() {
+        int result = SqMax.max(6, 5, 4, 7);
+        assertThat(result, is(7));
+    }
 }
