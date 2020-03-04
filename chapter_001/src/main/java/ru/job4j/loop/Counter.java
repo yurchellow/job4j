755eeaf3d9e4cc2f4d0514a2af1892433a01c371
yurchellow <onkelyura@gmail.com>
2020-03-04 11:45:59 +0300

    Counter+Test

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -3,8 +3,17 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int index = sum + start + finish; index <= start + finish; index++) {
+        for (int index = start; index <=finish; index++) {
             sum = sum + index;
+        }
+        return sum;
+    }
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index%2==0){
+                sum = sum + index;
+            }
         }
         return sum;
     }
@@ -13,7 +22,9 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
-        System.out.println(sum(7, 6));
-        System.out.println(sum(10, 5));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
---
+++ chapter_001/src/test/java/loop/CounterTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty1() {
+        int rsl = Counter.sumByEven(4, 8 );
+        int expected = 18;
+        assertThat(rsl, is(expected));
+    }
+}
