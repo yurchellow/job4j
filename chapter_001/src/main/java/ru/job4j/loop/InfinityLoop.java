dc26aac090a1bdb7fffe20a7107cfbca77448299
yurchellow <onkelyura@gmail.com>
2020-03-07 19:38:05 +0300

    Board

--- chapter_001/src/main/java/ru/job4j/loop/InfinityLoop.java
+++ chapter_001/src/main/java/ru/job4j/loop/InfinityLoop.java
@@ -4,7 +4,7 @@ public class InfinityLoop {
     public static void main(String[] args) {
         for (int index = 0; index < 10; index++) {
             System.out.println("infinity");
-            index = index * 1;
+            index = index - 1;
         }
     }
 }
