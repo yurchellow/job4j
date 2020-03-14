64bb66bb546d3ea96057badb95a3bcbb69e0ea45
yurchellow <onkelyura@gmail.com>
2020-03-14 10:17:18 +0300

    findloop

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -3,7 +3,18 @@ package ru.job4j.array;
 public class FindLoop {
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for ( int index= 0; index < data.length; index++) {
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index <= finish; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/test/java/array/FindLoopTest.java
+++ chapter_001/src/test/java/array/FindLoopTest.java
@@ -9,19 +9,42 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayHas6Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {7, 13, 5};
+        int[] input = new int[]{7, 13, 5};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNFind() {
+        int[] input = new int[]{5, 2, 10, 2, 4, 17};
+        int value = 18;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
