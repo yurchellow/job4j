4bf5a6582f52113663ad8a0c4f8ec45c2d28115f
yurchellow <onkelyura@gmail.com>
2020-03-14 10:00:41 +0300

    EndsWith+test

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length -1 -i] != word[word.length -1-i]) {
+                result = false;
+                break;
+            }
+        }
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        return result;
+    }
+}
