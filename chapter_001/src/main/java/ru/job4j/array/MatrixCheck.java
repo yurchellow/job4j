422b7db3465b4571ec9cd71c4ab7896fdbcc67c9
yurchellow <onkelyura@gmail.com>
2020-03-19 10:03:03 +0300

    MatrixCheck

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,13 +4,13 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board[row].length; i++) {
-                if (board[row][i] != board[row][0]) {
+            if (board[row][i] != board[row][0]) {
                 result = false;
                 break;
             }
-            }
-            return result;
         }
+        return result;
+    }
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
@@ -24,8 +24,18 @@ public class MatrixCheck {
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int i = 0; i < board.length; i++) {
-            rsl[i]=board[i][i];
+            rsl[i] = board[i][i];
         }
         return rsl;
     }
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
+}
